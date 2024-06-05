98d2faf927ad9aa2928743c0eef4ad87fd92ba77
BITCOM <BITCOM@BIT>
2024-06-05 17:17:57 +0900

    코드 정리

--- mysite3/src/main/java/com/poscodx/mysite/repository/UserRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/UserRepository.java
@@ -1,10 +1,5 @@
 package com.poscodx.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
@@ -21,36 +16,18 @@ public class UserRepository {
 	}
 	
 	
-	
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			
-			String url = "jdbc:mariadb://192.168.0.202:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} 
-		
-		return conn;
-	}
-	
-	
 	public int insert(UserVo vo) {
 		return sqlSession.insert("user.insert", vo);
-		
 	}
 
-
+	
 	public UserVo findByNoAndPassword(String email, String password) {
 		return sqlSession.selectOne(
 				"findByNoAndPassword"
 				, Map.of("email", email, "password", password));
 	}
 
+	
 	public UserVo findByNo(Long no) {
 		return sqlSession.selectOne("user.findByNo", no);
 	}
