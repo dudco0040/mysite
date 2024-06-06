e22f3ccdfcd7063fac735946c13121c8c84ad8ce
BITCOM <BITCOM@BIT>
2024-06-06 18:41:14 +0900

    for MyBatis Study..!

---
+++ mysite3_class/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -0,0 +1,104 @@
+package com.poscodx.mysite.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.poscodx.mysite.service.UserService;
+import com.poscodx.mysite.vo.UserVo;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	// 회원 가입
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "user/join";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping(value="/joinsuccess", method=RequestMethod.GET)  // redirect 
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
+	
+	// 로그인
+	@RequestMapping(value= "/login", method=RequestMethod.GET)
+	public String login() {
+		return "user/login";
+	}
+	
+	
+	@RequestMapping(value = "/login", method=RequestMethod.POST)
+	public String login(HttpSession session, UserVo vo, Model model) {
+		// login 처리- Atheutication
+		UserVo authUser = userService.getUser(vo.getEmail(), vo.getPassword());
+		
+		if(authUser == null) {
+			model.addAttribute("email", vo.getEmail());
+			model.addAttribute("reusult", "fail");
+			
+			return "user/login";
+		}
+		
+		// login 처리
+		session.setAttribute("authUser", authUser);
+		
+		return "redirect:/";
+	}
+	
+	// 로그아웃
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("authUser");
+		session.invalidate();
+		
+		return "redirect:/";
+	}
+	
+	
+	// 정보 수정
+	@RequestMapping(value="/update", method=RequestMethod.GET)
+	public String update(HttpSession session, Model model) {
+		// access control
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		if(authUser == null) {
+			return "redirect:/";
+		}
+		////////////////////////
+
+		UserVo vo = userService.getUser(authUser.getNo());
+		model.addAttribute("userVo", vo);
+
+		return "user/update";
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public String update(HttpSession session, UserVo vo) {
+		// access control
+		UserVo authUser = (UserVo)session.getAttribute("authUser");
+		if(authUser==null) {
+			return "redirect:/";   // 비정상적인 접근
+		}
+		////////////////////// 보안 - 횡단 관심
+
+		vo.setNo(authUser.getNo());
+		userService.update(vo);
+		
+		authUser.setName(vo.getName());
+		
+		return "redirect:/user/update";
+	}
+}
---
+++ mysite3_class/src/main/java/com/poscodx/mysite/repository/UserRepository.java
@@ -0,0 +1,42 @@
+package com.poscodx.mysite.repository;
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.poscodx.mysite.vo.UserVo;
+
+@Repository
+public class UserRepository {
+	private SqlSession sqlSession;
+	
+	public UserRepository(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+	
+	public int insert(UserVo vo) {
+		return sqlSession.insert("user.insert", vo);
+	}
+
+	
+	public UserVo findByNoAndPassword(String email, String password) {
+		return sqlSession.selectOne(
+				"findByNoAndPassword"
+				, Map.of("email", email, "password", password));
+	}
+
+	
+	public UserVo findByNo(Long no) {
+		return sqlSession.selectOne("user.findByNo", no);
+	}
+
+
+	public int update(UserVo vo) {	
+		return sqlSession.selectOne("user.udate", vo);
+
+	}
+	
+
+}
