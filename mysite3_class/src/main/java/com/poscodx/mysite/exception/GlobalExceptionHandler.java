21ed51ce9faf97a4ba6111430bdb6b6e60e77d5d
BITCOM <BITCOM@BIT>
2024-06-12 14:59:20 +0900

    6/12 Logging

--- mysite3_class/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite3_class/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.poscodx.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,15 +12,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 // exception 발생 시, 실행되는 코드
 public class GlobalExceptionHandler {
-	
+	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
 	// Advice
 	@ExceptionHandler(Exception.class)  // Exception의 종류
 	public String handler(Exception e, Model model) {
 		// 1. 로깅(logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
+		logger.error(errors.toString());
 		
-		System.out.println(errors.toString());   // console에 출력
+		//System.out.println(errors.toString());   // console에 출력  <appender-ref ref="consoleAppender" /> 추가
 		
 		// 2. 사과(종료)
 		model.addAttribute("error", errors.toString());
